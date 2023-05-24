5deb7842fdf8b40929f007db3524073b2c9de65a
Alex Kovalenko <106081917+K-Lesha@users.noreply.github.com>
2023-05-24 14:02:30 +0200

    Initial Commit

---
+++ RailwaySimulator/RailwaySimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RailwaySimulatorApp.swift
+//  RailwaySimulator
+//
+//  Created by Alex on 24.05.23.
+//
+
+import SwiftUI
+
+@main
+struct RailwaySimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
